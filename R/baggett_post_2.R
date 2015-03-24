496769f5e624f129541543c8cf7f1dcf2f2bc5b2
Aaron Baggett <aaronbaggett@gmail.com>
2015-03-23 21:35:29 -0500

    Add anthropometric dimensions fig

--- R/baggett_post_2.R
+++ R/baggett_post_2.R
@@ -25,19 +25,35 @@ ggplot(data = carp, aes(x = sz_bot)) + geom_density()
 
 heights <- read.csv("/Users/AB/Dropbox/Dissertation/Data/Anthropometric Data/Anthropometric_Measurements.csv")
 
+STATURE: height
+SUPRASTERNALE_HT: length from top of shoulders to ground
+WAIST_HT.OMPHALION: length from top of pants to ground
+
+
+WAIST_HT_NATURAL: length from ground to midpoint between shoulders and waist
+PATELLA.MID_HT: length from ground to knee
+
+feet <- as.numeric(as.character(substr(pfx_14$b_height, 1, 1)))*12
+feet <- feet + as.numeric(as.character(substr(pfx_14$b_height, 3, 3)))
+
+pfx_14$b_height <- feet
 
 ansur <- read.delim("~/baseball_blog/data/ansur_men.txt", header = TRUE)
 
 ansur <- ansur %>% 
   select(STATURE, SUPRASTERNALE_HT, WAIST_HT_NATURAL, PATELLA.MID_HT)
 
-ansur*0.0393701
+ansur <- compute(ansur*0.0393701)
 
-STATURE: height
-SUPRASTERNALE_HT: length from top of shoulders to ground
-WAIST_HT.OMPHALION: length from top of pants to ground
-WAIST_HT_NATURAL: length from midpoint between shoulders and waist and ground
-OMPHALION: length between shoulders and waist
-PATELLA.MID_HT: length from knee to ground
+ansur$STATURE <- round(ansur$STATURE)
 
-ansur$
+ansur_hts <- ansur %>% 
+  select(b_height = STATURE, WAIST_HT_NATURAL, PATELLA.MID_HT) %>% 
+  group_by(b_height) %>%
+  summarize(ht_top = mean(WAIST_HT_NATURAL)/12, ht_bott = mean(PATELLA.MID_HT)/12)
+
+pfx_14 <- left_join(pfx_14, ansur_hts)
+
+
+
+
